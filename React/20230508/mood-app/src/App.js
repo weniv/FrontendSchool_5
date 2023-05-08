f1a9bb7f64b3b71167b601ce768ec03f544359e3
nugurejeil <nugurejeil@ucsit.co.kr>
2023-05-08 15:51:16 +0900

    상태앱 프롭스 연결

--- React/20230508/mood-app/src/App.js
+++ React/20230508/mood-app/src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import DisplayMood from "./Components/DisplayMood/DisplayMood";
 import MenuList from "./Components/MenuList/MenuList";
 
 function App() {
+
+  const [currentMood, setCurrentMood] = useState("");
+
   return (
     <div>
-      <MenuList />
-      <DisplayMood />
+      <MenuList mood={currentMood} onItemClick={setCurrentMood} />
+      <DisplayMood mood={currentMood} />
     </div>
   );
 }
--- React/20230508/mood-app/src/Components/MenuList/MenuList.jsx
+++ React/20230508/mood-app/src/Components/MenuList/MenuList.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import MenuListItem from '../MenuListItem/MenuListItem'
 import './MenuList.css'
 
-export default function MenuList() {
+export default function MenuList(props) {
     const menus = ["좋아요!", "정말 좋아요!", "최고에요!", "미쳤어요!!"];
 
     return (
         <ul className='container-list'>
             {menus.map((moodEl) => {
-                return <MenuListItem mood={moodEl} />
+                return <MenuListItem mood={moodEl} isSelected={props.mood === moodEl} onClick={props.onItemClick} />
             })}
         </ul>
     )
