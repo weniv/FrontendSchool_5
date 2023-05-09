57c1c1da461f374e8ac6fd3d226ed4ec658c8184
nugurejeil <nugurejeil@ucsit.co.kr>
2023-05-09 16:09:15 +0900

    모달완료

--- React/20230509/reactevent/src/App.js
+++ React/20230509/reactevent/src/App.js
@@ -3,6 +3,23 @@ import Login from './Login';
 import Homepage from "./Homepage";
 import Modal from "./Modal";
 
+
+function Hello(props) {
+  const name = props.name;
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //1~10호를 만들 것입니다.
+
+  // list map을 이용하여 h1 태그들의 리스트를 만듭니다.
+
+
+  return (
+    <div>
+      {num.map((i) => <h1>안녕, {name} {i}호</h1>)}
+    </div>
+  )
+}
+
+
+
 function App() {
   const user = {
     idUser: 'jaehyun@weniv.com',
@@ -10,16 +27,22 @@ function App() {
   }
 
   const [login, setLogin] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+
+  function modalClose() {
+    setModalShow(false);
+  }
 
   console.log(login);
   return (
     <>
-      login ? <Homepage /> : < Login infoUser={user} setLogin={setLogin} />
-      < Modal>
+      <Hello name="licat" />
+      {login ? <Homepage /> : < Login infoUser={user} setLogin={setLogin} />}
+      {modalShow && < Modal modalClose={modalClose}>
         <h2>사용약관에 대해 말씀드리겠습니다.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illum laborum, quo facilis eaque quam optio aliquam nam provident. Laboriosam delectus, atque doloremque nam harum molestias ab quisquam! Quis, pariatur.</p>
         <a href="#none">더 보기</a>
-      </Modal>
+      </Modal>}
     </>
   )
 
--- React/20230509/reactevent/src/Modal.jsx
+++ React/20230509/reactevent/src/Modal.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Modal.css'
 
-export default function Modal({ children }) {
+export default function Modal({ children, modalClose }) {
     return (
         <div className='modal-backdrop'>
             <article className='modal'>
                 {children}
                 {/* <h2>홈페이지에 오신것을 환영합니다!</h2>
                 <p>좋은하루 되세요!</p> */}
-                <button>닫기</button>
+                <button onClick={modalClose}>닫기</button>
             </article>
         </div>
     )
