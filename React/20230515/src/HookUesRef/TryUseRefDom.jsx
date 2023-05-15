0f5a3d1510e4ba8e2dae9df26f5b19ace227e24b
WonBeomChoi <cwb2310@gmail.com>
2023-05-15 15:09:30 +0900

    쉽다쉬워 ref로 dom조작하기

--- React/20230515/src/HookUesRef/TryUseRefDom.jsx
+++ React/20230515/src/HookUesRef/TryUseRefDom.jsx
@@ -3,29 +3,25 @@ import React, { useState, useRef } from "react";
 const TryUseRefDom = () => {
   const [emailValue, setEmailValue] = useState(""); // email state 값
   const [pwValue, setPwValue] = useState(""); // pw state 값
-  // const a = ''
-  // const b = ''
+
+  const emailInput = useRef(null); // email input에 대한 useRef
+  const pwInput = useRef(null); // pw input에 대한 useRef
 
   const inputCheck = (e) => {
     e.preventDefault();
-    console.log(document.querySelectorAll("input"));
-    setEmailValue(document.querySelectorAll("input")[0].value);
-    setPwValue(document.querySelectorAll("input")[1].value);
-    // setEmailValue(emailInput.current.value)
-    // setPwValue(pwInput.current.value)
+    console.log("로그인 버튼 누름");
+    console.log(emailInput.current.value, pwInput.current.value);
+    setEmailValue(emailInput.current.value);
+    setPwValue(pwInput.current.value);
   };
 
   return (
     <form style={{ display: "flex", flexDirection: "column" }}>
       <label>
-        이메일 : <input type="email" />
-        {/* 이메일 : <input type="email" ref={a} /> */}
-        {/* 이메일 : <input type="email" ref={emailValue}/> */}
+        이메일 : <input type="email" ref={emailInput} />
       </label>
       <label>
-        비밀번호 : <input type="password" />
-        {/* 비밀번호 : <input type="password" ref={b} /> */}
-        {/* 비밀번호 : <input type="password" ref={pwValue} /> */}
+        비밀번호 : <input type="password" ref={pwInput} />
       </label>
 
       <button type="submit" style={{ width: "100px" }} onClick={inputCheck}>
