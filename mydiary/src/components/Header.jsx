65b2c2b9a52fae5fe82365aba6bc96e1757ba39d
nugurejeil <nugurejeil@ucsit.co.kr>
2023-06-07 12:08:45 +0900

    firebase start

---
+++ mydiary/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+import logo from '../img/logo.svg'
+
+export default function Header() {
+    return (
+        <header>
+            <div className={styles["header-wrap"]}>
+                <h1>
+                    <Link to="/">
+                        <img className="logo" src={logo} alt="두근두근 비밀일기" />
+                    </Link>
+                </h1>
+                <div>
+                    <Link to="/signup" className="btn-join">회원가입</Link>
+                </div>
+            </div>
+        </header>
+    )
+}
---
+++ mydiary/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import ReactDOM from 'react-dom'; //구버전
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import 'normalize.css'
+import './index.css'
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
