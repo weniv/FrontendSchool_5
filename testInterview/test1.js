97822ef258942667b6b1118d6f58bddcba8945b9
nugurejeil <nugurejeil@ucsit.co.kr>
2023-06-30 15:04:36 +0900

    인터뷰준비

---
+++ testInterview/test1.js
@@ -0,0 +1,2 @@
+document.querySelector('h1').style.color = 'olive';
+console.log('test1.js is done');
---
+++ testInterview/test2.js
@@ -0,0 +1,2 @@
+document.querySelector('h1').style.color = 'teal';
+console.log('test2.js is done');
